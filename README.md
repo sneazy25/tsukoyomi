299061258d212aad8731109ff41ab34d61002857
sneazy25 <rahmi.deril@gmail.com>
2023-03-01 13:20:09 +0700

    InstagramAutoPoster

---
+++ README.md
@@ -0,0 +1,22 @@
+# InstagramAutoPoster
+
+## About
+
+I made this program for multiple reasons. The first was to learn how to use APIs and for general programming practice. I do not use Python a lot because everyone knows Java is the superior language. The second reason was because I saw someone selling the same program for $10/month, so I thought: "why not open source it."  
+
+### How to Use
+
+1. Install Python 3.x
+2. Install all the required modules `pip(3) install -r requirements.txt`
+3. Edit `config.json` and change the `username`, `password`, `caption`, and `interval`
+4. Run `python(3) poster.py` in your terminal
+5. Sit back and relax!
+
+### To-Do
+  
+  - Make a GUI
+  - Add more features
+  
+### Known Issues
+
+  - Nothing because my programs are perfect
---
+++ poster.py
@@ -0,0 +1,47 @@
+import os, random, json, requests, sched
+from InstagramAPI import InstagramAPI
+from time import sleep, time
+from requests_toolbelt import MultipartEncoder
+
+def readConfig():
+    filename = "config.json"
+    if filename:
+        with open(filename, 'r') as f:
+            cfg = json.load(f)
+    return cfg  
+
+def loginIG(username, password):
+    ig = InstagramAPI(username, password)
+
+    if (ig.login()):
+        print("Logged into Instagram")
+    else:
+        print("Failed to log into Instagram")
+    
+    return ig
+
+def postPhoto(interval, caption, ig_api):
+    rndPhoto = random.choice(os.listdir("photos"))
+    rndPath = 'photos/' + rndPhoto
+
+    post = ig_api.uploadPhoto(rndPath, caption)
+    
+    if post:
+        print("posted photo")
+    else:
+        print("post failed")
+    os.remove(rndPath)
+    print("deleted " + rndPhoto)
+
+def main():
+    igUsername = readConfig()['username']
+    igPassword = readConfig()['password']
+    igCaption = readConfig()['caption']
+    postInterval = readConfig()['interval']
+
+    api = loginIG(igUsername, igPassword) # Log into Instagram
+    s = sched.scheduler(time, sleep) # Create scheduler for schedueled posts
+    s.enter(60, 1, postPhoto(postInterval, igCaption, api), (s,))  # (delay, priority, action, argument)
+
+if __name__ == "__main__":
+    main()
